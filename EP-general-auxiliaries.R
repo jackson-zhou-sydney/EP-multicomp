0aded5cc2a5a58af9d2a88d87329be4459b268d2
Jackson Zhou <jackson.zhou@sydney.edu.au>
2022-10-09 11:45:33 +1100

    Added code for lasso EP.

--- EP-general-auxiliaries.R
+++ EP-general-auxiliaries.R
@@ -1,10 +1,10 @@
 
 # General auxiliary functions and variables for expectation propagation
 
-library(tidyverse) # Data manipulation and plotting
-library(glmnet)    # Fast ridge regression estimates
-library(rstan)     # Robust MCMC and diagnostics
-library(EnvStats)  # Compute empirical densities
+err <- function(e) {
+  # Return NA on error
+  return(NA)
+}
 
 force.sym <- function(m) {
   # Force matrix to be symmetric
@@ -13,47 +13,56 @@ force.sym <- function(m) {
   return(m.sym)
 }
 
-GI.0 <- function(a, b, c) {
+GI.0 <- function(x) {
   # Gaussian integral (0th raw moment)
+  a <- x[1]; b <- x[2]; c <- x[3]
   sqrt(2*pi/a)*exp(-0.5*(c - b^2/(4*a)))
 }
 
-GI.1 <- function(a, b, c) {
+GI.1 <- function(x) {
   # Gaussian integral (1st raw moment)
+  a <- x[1]; b <- x[2]; c <- x[3]
   sqrt(2*pi/a)*(-b/(2*a))*exp(-0.5*(c - b^2/(4*a)))
 }
 
-GI.2 <- function(a, b, c) {
+GI.2 <- function(x) {
   # Gaussian integral (2nd raw moment)
+  a <- x[1]; b <- x[2]; c <- x[3]
   sqrt(2*pi/a)*(1/a + b^2/(4*a^2))*exp(-0.5*(c - b^2/(4*a)))
 }
 
-TGI.lower.0 <- function(a, b, c) {
+TGI.lower.0 <- function(x) {
   # Truncated Gaussian integral (lower, 0th raw moment)
-  exp(log(sqrt(2*pi/a)) - 0.5*(c - b^2/(4*a)) + pnorm(0, -b/(2*a), sqrt(1/a), log.p = TRUE))
+  a <- x[1]; b <- x[2]; c <- x[3]
+  exp(log(sqrt(2*pi/a)) - 0.5*(c - b^2/(4*a)) + pnorm(0, -b/(2*a), sqrt(1/a), log.p = T))
 }
 
-TGI.lower.1 <- function(a, b, c) {
+TGI.lower.1 <- function(x) {
   # Truncated Gaussian integral (lower, 1st raw moment)
-  sqrt(2*pi/a)*(-(b/(2*a))*exp(-0.5*(c - b^2/(4*a)) + pnorm(b/(2*sqrt(a)), log.p = TRUE)) - (1/sqrt(a))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = TRUE)))
+  a <- x[1]; b <- x[2]; c <- x[3]
+  sqrt(2*pi/a)*(-(b/(2*a))*exp(-0.5*(c - b^2/(4*a)) + pnorm(b/(2*sqrt(a)), log.p = T)) - (1/sqrt(a))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = T)))
 }
 
-TGI.lower.2 <- function(a, b, c) {
+TGI.lower.2 <- function(x) {
   # Truncated Gaussian integral (lower, 2nd raw moment)
-  sqrt(2*pi/a)*((b^2/(4*a) + 1)*exp(-0.5*(c - b^2/(4*a)) + pnorm(b/(2*sqrt(a)), log.p = TRUE)) + (b/(2*sqrt(a)))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = TRUE)))/a
+  a <- x[1]; b <- x[2]; c <- x[3]
+  sqrt(2*pi/a)*((b^2/(4*a) + 1)*exp(-0.5*(c - b^2/(4*a)) + pnorm(b/(2*sqrt(a)), log.p = T)) + (b/(2*sqrt(a)))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = T)))/a
 }
 
-TGI.upper.0 <- function(a, b, c) {
+TGI.upper.0 <- function(x) {
   # Truncated Gaussian integral (upper, 0th raw moment)
-  exp(log(sqrt(2*pi/a)) - 0.5*(c - b^2/(4*a)) + pnorm(0, -b/(2*a), sqrt(1/a), lower.tail = FALSE, log.p = TRUE))
+  a <- x[1]; b <- x[2]; c <- x[3]
+  exp(log(sqrt(2*pi/a)) - 0.5*(c - b^2/(4*a)) + pnorm(0, -b/(2*a), sqrt(1/a), lower.tail = F, log.p = T))
 }
 
-TGI.upper.1 <- function(a, b, c) {
+TGI.upper.1 <- function(x) {
   # Truncated Gaussian integral (upper, 1st raw moment)
-  sqrt(2*pi/a)*(-(b/(2*a))*exp(-0.5*(c - b^2/(4*a)) + pnorm(-b/(2*sqrt(a)), log.p = TRUE)) + (1/sqrt(a))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = TRUE)))
+  a <- x[1]; b <- x[2]; c <- x[3]
+  sqrt(2*pi/a)*(-(b/(2*a))*exp(-0.5*(c - b^2/(4*a)) + pnorm(-b/(2*sqrt(a)), log.p = T)) + (1/sqrt(a))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = T)))
 }
 
-TGI.upper.2 <- function(a, b, c) {
+TGI.upper.2 <- function(x) {
   # Truncated Gaussian integral (upper, 2nd raw moment)
-  sqrt(2*pi/a)*((b^2/(4*a) + 1)*exp(-0.5*(c - b^2/(4*a)) + pnorm(-b/(2*sqrt(a)), log.p = TRUE)) - (b/(2*sqrt(a)))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = TRUE)))/a
+  a <- x[1]; b <- x[2]; c <- x[3]
+  sqrt(2*pi/a)*((b^2/(4*a) + 1)*exp(-0.5*(c - b^2/(4*a)) + pnorm(-b/(2*sqrt(a)), log.p = T)) - (b/(2*sqrt(a)))*exp(-0.5*(c - b^2/(4*a)) + dnorm(b/(2*sqrt(a)), log = T)))/a
 }
