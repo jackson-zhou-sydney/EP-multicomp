387ede83aa6bb72c1cc20d4a2bd03f4c3b097133
Jackson Zhou <jackson.zhou@sydney.edu.au>
2022-11-25 17:57:45 +1100

    Added means and covariances to results.

--- Quantile/Quantile-simbench.R
+++ Quantile/Quantile-simbench.R
@@ -346,5 +346,6 @@ for (type.iter in 1:num.each.type) {
                                                    mfvb.mu = as.vector(mfvb.mu), mfvb.Sigma = mfvb.Sigma)
 }
 
-save(sim.res.df.1, sim.res.df.2, sim.res.df.3, sim.res.df.4, bench.res.df.1, bench.res.df.2, bench.res.df.3, bench.res.df.4,
+save(sim.res.df.1, sim.res.df.2, sim.res.df.3, sim.res.df.4, sim.res.list,
+     bench.res.df.1, bench.res.df.2, bench.res.df.3, bench.res.df.4, bench.res.list,
      file = "Quantile/Quantile-results.RData")
