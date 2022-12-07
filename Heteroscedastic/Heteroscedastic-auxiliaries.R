3f1cdd710c38b898b751a27a5e38037f19cc8821
Jackson Zhou <jackson.zhou@sydney.edu.au>
2022-12-07 13:13:07 +1100

    Fixed typo for ncol.

--- Heteroscedastic/Heteroscedastic-auxiliaries.R
+++ Heteroscedastic/Heteroscedastic-auxiliaries.R
@@ -25,7 +25,7 @@ log.joint.likelihood <- function(theta, X.1, X.2, y, mu.theta, Sigma.theta) {
 
 laplace.approx <- function(X.1, X.2, y, mu.theta, Sigma.theta, lambda.init, maxit) {
   # Laplace approximation
-  p.1 <- ncol(X.2)
+  p.1 <- ncol(X.1)
   p.2 <- ncol(X.2)
   
   start <- if (p.1 > 2) c(as.vector(coef(glmnet(X.1[, -1], y, alpha = 0, lambda = lambda.init))), rep(0, p.2)) else
