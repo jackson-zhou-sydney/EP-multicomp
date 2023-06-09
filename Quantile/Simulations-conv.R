054750474d05491610a722cf8a4a26b4032575d3
Jackson Zhou <jackson.zhou@sydney.edu.au>
2023-06-09 14:03:23 +1000

    Modularised quantile convergence checks.

--- Quantile/Simulations-conv.R
+++ Quantile/Simulations-conv.R
@@ -7,6 +7,8 @@ source("Quantile/Auxiliaries.R")
 args <- commandArgs(trailingOnly = T)
 seed <- as.numeric(args[1])
 set.seed(seed)
+mcmc.iter <- as.numeric(args[2])
+mcmc.warmup <- warmup.mult*mcmc.iter
 
 library(cmdstanr)
 mcmc <- cmdstan_model("Quantile/Models/MCMC.stan")
@@ -31,33 +33,29 @@ for (type.iter in 1:num.sim) {
   mu.kappa <- mu.theta[p + 1]
   sigma.2.kappa <- Sigma.theta[p + 1, p + 1]
   
-  for (k in 1:length(mcmc.check.iter)) {
-    print(paste0("Current check.iter: ", mcmc.check.iter[k]))
-    
-    stan.res <- mcmc$sample(data = list(N = n,
-                                        p = p,
-                                        X = X,
-                                        y = y,
-                                        mu_theta = mu.theta,
-                                        Sigma_theta = Sigma.theta,
-                                        tau = tau), 
-                            seed = seed, 
-                            chains = num.cores, 
-                            parallel_chains = num.cores,
-                            iter_sampling = mcmc.check.iter[k],
-                            iter_warmup = mcmc.check.warmup[k],
-                            refresh = 1)
-    
-    mcmc.summary <- stan.res$summary()
-    
-    for (j in 1:(p + 1)) {
-      sim.r.hat.df <- sim.r.hat.df %>% add_row(seed = seed,
-                                               sim = type.iter,
-                                               mcmc_iter = mcmc.check.iter[k],
-                                               j = j,
-                                               r_hat = mcmc.summary %>% filter(variable == paste0("theta[", j, "]")) %>% pull(rhat))
-    }
+  stan.res <- mcmc$sample(data = list(N = n,
+                                      p = p,
+                                      X = X,
+                                      y = y,
+                                      mu_theta = mu.theta,
+                                      Sigma_theta = Sigma.theta,
+                                      tau = tau), 
+                          seed = seed, 
+                          chains = num.cores, 
+                          parallel_chains = num.cores,
+                          iter_sampling = mcmc.iter,
+                          iter_warmup = mcmc.warmup,
+                          refresh = 1)
+  
+  mcmc.summary <- stan.res$summary()
+  
+  for (j in 1:(p + 1)) {
+    sim.r.hat.df <- sim.r.hat.df %>% add_row(seed = seed,
+                                             sim = type.iter,
+                                             mcmc_iter = mcmc.iter,
+                                             j = j,
+                                             r_hat = mcmc.summary %>% filter(variable == paste0("theta[", j, "]")) %>% pull(rhat))
   }
 }
 
-save(sim.r.hat.df, file = paste0("Quantile/Results/Simulations-conv-results-", str_pad(seed, 2, pad = "0"), ".Rdata"))
+save(sim.r.hat.df, file = paste0("Quantile/Results/Simulations-conv-results-", mcmc.iter, "-", str_pad(seed, 2, pad = "0"), ".Rdata"))
