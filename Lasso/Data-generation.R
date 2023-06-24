146bdeeab693f6b4410f6e6111489c3a9e6d1cd8
Jackson Zhou <jackson.zhou@sydney.edu.au>
2023-06-25 08:46:35 +1000

    Removed filter for energy data.

--- Lasso/Data-generation.R
+++ Lasso/Data-generation.R
@@ -45,8 +45,7 @@ save(X, y, file = "Lasso/Data/Benchmarks/Bench-3.RData")
 ## Benchmark 4
 
 energy <- read.csv("Benchmark-data/energydata_complete.csv")
-energy_filtered <- energy[sample(1:nrow(energy), 3000), ]
-energy_cleaned <- as.data.frame(scale(energy_filtered[, -c(1, 3, 28, 29)]))
+energy_cleaned <- as.data.frame(scale(energy[, -c(1, 3, 28, 29)]))
 
 energy_squared <- energy_cleaned %>% 
   mutate_all(function(x) x^2) %>% 
